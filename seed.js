04ff8dfb514b896237d803faf21dd97be58ccf9c
el-fer <fer@innovaresip.com>
2019-11-18 14:24:09 -0600

    first version

---
+++ seed.js
@@ -0,0 +1,51 @@
+export const observations = [
+	{
+		id: 1,
+		date: "11/13/2019",
+		notes: "this is a past observation",
+		status: "completed",
+		score: 100
+	},
+	{
+		id: 2,
+		date: "11/14/2019",
+		notes: "this is also a past observation",
+		status: "completed",
+		score: 200
+	},
+	{
+		id: 3,
+		date: "11/15/2019",
+		notes: "this is an in-progress observation",
+		status: "in-progress",
+		score: "0"
+	},
+	{
+		id: 4,
+		date: "11/20/2019",
+		notes: "this is a future observation",
+		status: "new",
+		score: ""
+	}
+]
+
+export const users = [
+	{
+		id: 1,
+		name: "Mr. Smith",
+		gradeLevels: 2,
+		courses: "Math"
+	},
+	{
+		id: 2,
+		name: "Mrs. Jimenez",
+		gradeLevels: 5,
+		courses: "History"
+	},
+	{
+		id: 3,
+		name: "Ms. Ortiz",
+		gradeLevels: 7,
+		courses: "Biology"
+	}
+]
